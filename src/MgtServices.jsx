458e3acccfd4a5556a6d3857eeebec16af0327ef
Clarence A Royal <croyal@comcast.net>
2022-05-13 10:25:18 -0400

    update text

--- src/MgtServices.jsx
+++ src/MgtServices.jsx
@@ -11,7 +11,7 @@ return (
         </p>
         <p>
             Royal Consulting is a family run property management company that has been in operation for 3+ years.
-            We work with local realtors, real estte brokers, tenants and landlords to help property owners to 
+            We work with local realtors, real estate brokers, tenants and landlords to help property owners to 
             maximize their profits and tenants acquire affordable housing. If you would like to know more about our services please contact us, 
             we look forward to hearing from you.
         </p>
@@ -23,7 +23,7 @@ return (
             <h3>
                 Inspections
             </h3>
-            <p>We want to make sure yur property stays in top shape, so we conduct periodic inspections to ensure the tenants are mainaining your property as required.
+            <p>We want to make sure yur property stays in top shape, so we conduct periodic inspections to ensure the tenants are maintaining your property as required.
                We also attend all necessary municipal inspections for you as well.
             </p>
 
@@ -31,7 +31,7 @@ return (
                 Tenant Placement
             </h3>
             <p>We market your rental properties, coordinate and attend all of the showings, identify the best applicants, and draft and execute the lease for you.
-            We screen all tenants for credit, criminal, and eviction history to try and get you the best tenants possible.</p>
+            We screen all tenants for credit, criminal, and eviction history to try and get you the best tenants possible. </p>
             
             <h3>Reporting</h3>
             <p>We send you an itemized owner's statement each month detailing all the income and expenses associated with your property. 
@@ -39,11 +39,11 @@ return (
 
             <h3>Maintenance</h3>
             <p>We manage all property maintenance and offer you unbeatable pricing and service.
-                We have an extensive network of contractors that can tackle any repair. Whether it is fixing a leaky faucet or a complet renovation, we can coordinate it all.
+                We have an extensive network of contractors that can tackle any repair. Whether it is fixing a leaky faucet or a complete renovation, we can coordinate it all.
                 That means no tenants calling you at all hours of the day asking for maintenance.
             </p>
             <h3>Compliance</h3>
-            We make sure you and your property stay up to dte with all licensing requirements and comply with all local, state and deferal laws. 
+            We make sure you and your property stay up to date with all licensing requirements and comply with all local, state and deferral laws. 
             Our leases are clear, concise, attorney reviewed and enforceable in court.
             We keep accurate ledgers of all tenant deposits and payments.
 
